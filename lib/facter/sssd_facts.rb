2ad12dc263a664e2318d015d7a4d621afdb13b4e
Nicolas Vanelslande <nicolas.vanelslande@gmail.com>
2017-07-25 10:50:38 +0100

    ssd_facts: Do Nothing if augeas cannot be loaded.
    augeas cannot be loaded on Windows

--- lib/facter/sssd_facts.rb
+++ lib/facter/sssd_facts.rb
@@ -1,21 +1,25 @@
-require 'facter'
-require 'augeas'
+begin
+  require 'facter'
+  require 'augeas'
 
-Facter.add('sssd_version') do
-  setcode do
-    Facter::Util::Resolution.exec('sssd --version')
-  end
-end
-
-Facter.add(:sssd_services) do
-  setcode do
-    if File.exist? '/etc/sssd/sssd.conf'
-      Augeas::open do |aug|
-        aug.load
-        aug.get("/files/etc/sssd/sssd.conf/target[.='sssd']/services")
-      end
-    else
-      'nss, pam, ssh'
+  Facter.add('sssd_version') do
+    setcode do
+      Facter::Util::Resolution.exec('sssd --version')
     end
   end
+
+  Facter.add(:sssd_services) do
+    setcode do
+      if File.exist? '/etc/sssd/sssd.conf'
+        Augeas::open do |aug|
+          aug.load
+          aug.get("/files/etc/sssd/sssd.conf/target[.='sssd']/services")
+        end
+      else
+        'nss, pam, ssh'
+      end
+    end
+  end
+rescue LoadError
+  # Do Nothing if augeas cannot be loaded. (may be resolving facts on Windows)
 end
